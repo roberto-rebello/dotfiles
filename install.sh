43b4ca2c9de926b81cbe134259559c0944c3176d
Roberto Rebello <roberto.rebello@gmail.com>
2022-01-08 22:07:42 -0300

    Added i3blocks and moved i3 packages do separate install

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ sudo apt-get install -y curl
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
-sudo apt-get update && sudo apt-get install -y i3 feh playerctl htop vim git tmux meld discord signal-desktop spotify-client steam
+sudo apt-get update && sudo apt-get install -y htop vim git tmux meld discord signal-desktop spotify-client steam
+sudo apt-get install -y i3 i3blocks feh playerctl
 
 cp -rv .atom/ .config/ .fonts/ .vimrc $HOME
